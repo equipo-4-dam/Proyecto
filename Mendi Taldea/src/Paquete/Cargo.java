9f6c4b328e89897a05fa279eec064529d65d4463
Santiago Gonzalez Correa <sirlocus66@gmail.com>
2019-05-07 18:46:55 +0200

    A ver si empezamos bien

---
+++ Mendi Taldea/src/Paquete/Cargo.java
@@ -0,0 +1,15 @@
+package Paquete;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Cargo {
+
+    //atributos
+    private int idCargo;
+    private String tipo;
+
+    //relaciones
+    private final List<Socio> socios = new ArrayList<>();
+
+}
---
+++ Mendi Taldea/src/Paquete/VentanaPrincipal.java
@@ -0,0 +1,12 @@
+package Paquete;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class VentanaPrincipal {
+
+    List<Socio> socios = new ArrayList<>();
+    //
+    //hola
+
+}
