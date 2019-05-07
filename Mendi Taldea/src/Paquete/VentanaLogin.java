fa4319f1ee3213b06c25b9c2c672c7d2f419cbb9
Irune <irune.mendez@ikasle.egibide.org>
2019-05-07 21:13:50 +0200

    Arreglando gitignore

---
+++ Mendi Taldea/src/Paquete/VentanaLogin.java
@@ -0,0 +1,32 @@
+package Paquete;
+
+import javax.swing.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class VentanaLogin {
+
+    public static final List<Socio> socios = new ArrayList<>();
+
+    //
+    //hola
+
+    private JPanel panel;
+    private JLabel JLtitulo;
+    private JPanel JPdatos;
+    private JTextField JTusuario;
+    private JLabel JLusuario;
+    private JLabel JLpass;
+    private JPasswordField JPpass;
+    private JButton JBlogin;
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("VentanaPrincipal");
+        frame.setContentPane(new VentanaLogin().panel);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+
+
+    }
+}
--- Mendi Taldea/src/Paquete/VentanaPrincipal.java
+++ Mendi Taldea/src/Paquete/VentanaPrincipal.java
@@ -1,24 +1,9 @@
 package Paquete;
 
 import javax.swing.*;
-import java.util.ArrayList;
-import java.util.List;
 
 public class VentanaPrincipal {
-
-    public static final List<Socio> socios = new ArrayList<>();
-
-    //
-    //hola
-
     private JPanel panel;
-    private JLabel JLtitulo;
-    private JPanel JPdatos;
-    private JTextField JTusuario;
-    private JLabel JLusuario;
-    private JLabel JLpass;
-    private JPasswordField JPpass;
-    private JButton JBlogin;
 
     public static void main(String[] args) {
         JFrame frame = new JFrame("VentanaPrincipal");
@@ -26,7 +11,6 @@ public class VentanaPrincipal {
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.pack();
         frame.setVisible(true);
-
-
+        frame.setSize(500,500);
     }
 }
